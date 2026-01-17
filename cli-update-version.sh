7cf56e2494ae76627a56a3b22084bee857b5f858
Simon Goller <neosam+github@posteo.de>
2026-01-17 10:30:08 +0100

    Fix cli update version script

--- cli-update-version.sh
+++ cli-update-version.sh
@@ -9,11 +9,11 @@ NEW_VERSION="$1"
 FOLLOWING_VERSION="${2}-dev"
 BRANCH="${3:-main}"
 
-#./update_versions.sh "$NEW_VERSION"
-#cargo build
-#jj commit -m "Set version to $NEW_VERSION"
-#jj b m "$BRANCH" --to @-
-#jj git push
+./update_versions.sh "$NEW_VERSION"
+cargo build
+jj commit -m "Set version to $NEW_VERSION"
+jj b m "$BRANCH" --to @-
+jj git push
 git tag -a "v$NEW_VERSION" "$BRANCH"
 git push --tags
 
