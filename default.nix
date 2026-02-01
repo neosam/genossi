3b3f936c436363e0c9447a54d40b729cd53cc56f
Simon Goller <neosam+github@posteo.de>
2026-02-01 23:34:07 +0100

    Set version to 1.2.5

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let
 in
   rustPlatform.buildRustPackage {
     pname = "inventurly-service";
-    version = "1.3.0-dev";
+    version = "1.2.5";
     src = src;
     nativeBuildInputs = with specificPkgs; [curl pkg-config openssl];
     buildInputs = with specificPkgs; [sqlite openssl];
