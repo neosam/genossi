f8860f82a14edc111c85963c565be365bf5da644
Simon Goller <neosam+github@posteo.de>
2026-06-07 20:44:38 +0200

    Set version to 2026.158.2

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ let
 in
   rustPlatform.buildRustPackage {
     pname = "genossi-service";
-    version = "2026.158.2-dev";
+    version = "2026.158.2";
     src = src;
     nativeBuildInputs = with specificPkgs; [curl pkg-config openssl];
     buildInputs = with specificPkgs; [sqlite openssl];
